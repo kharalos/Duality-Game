92562706f8640acb23c4f3644474477f632aaf0e
Ömer Ünal <ounal97@gmail.com>
2022-04-23 11:43:53 +0300

    update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -22,13 +22,21 @@ public class Enemy : MonoBehaviour
     private void Update()
     {
         if ((target.position - transform.position).magnitude < 40 && !pursue) pursue = true;
+    }
+    private void FixedUpdate()
+    {
         if (pursue)
         {
-            rb.AddForce((target.position - transform.position).normalized * 10);
+            rb.AddForce((target.position - transform.position).normalized * 20f);
 
             if ((target.position - transform.position).magnitude > 30)
             {
-                rb.AddForce((target.position - transform.position).normalized * 1000);
+                rb.velocity = (target.position - transform.position).normalized * 5f;
+            }
+
+            if ((target.position - transform.position).magnitude < 6)
+            {
+                rb.AddForce((target.position - transform.position).normalized * 0.05f, ForceMode.VelocityChange);
             }
         }
     }
@@ -56,7 +64,7 @@ public class Enemy : MonoBehaviour
         Debug.Log(gameObject.name + " has died.");
         GetComponent<AudioSource>().PlayOneShot(dieSound);
         FindObjectOfType<CharacterManager>().HealthChange(+50);
-        Destroy(gameObject, 4f);
+        Destroy(gameObject, 3f);
     }
 
     public virtual void Stop()
